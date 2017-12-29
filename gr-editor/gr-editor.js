d213d6429f8cdedc2d8e1b5445e7ef2c629f3b0c
Paladox none <thomasmulhall410@yahoo.com>
2017-12-29 16:12:22 +0000

    Support syntax highlighting
    Also maps the API preferences to proper codemirror properties... See
    https://github.com/GerritCodeReview/gerrit/blob/6c1a52b8698d1ad392c590e7b515f2b5668e73f4/gerrit-gwtui/src/main/java/com/google/gerrit/client/editor/EditScreen.java#L555
    
    Change-Id: Icc222cde89eff7a0992fbcaaf8e7bac560347c18

--- gr-editor/gr-editor.js
+++ gr-editor/gr-editor.js
@@ -14,6 +14,12 @@
 (function() {
   'use strict';
 
+  // we need to be on codemirror 5.33.0+ to get the support for
+  // text/x-php in CodeMirror.findModeByMIME
+  const LANGUAGE_MAP = {
+    'text/x-php': 'php',
+  };
+
   Polymer({
     is: 'gr-editor',
     /**
@@ -24,15 +30,16 @@
 
     properties: {
       fileContent: String,
+      fileType: String,
       mirror: Object,
       prefs: Object,
     },
 
     attached() {
       this.scopeSubtree(this.$.wrapper, true);
-      this.mirror = CodeMirror(this.$.wrapper, Object.assign({} , {
-        value: this.fileContent,
-      }, this.prefs));
+      const params =
+          this.getCodeMirrorParams(this.fileType, this.fileContent, this.prefs);
+      this.mirror = CodeMirror(this.$.wrapper, params);
       this.async(() => { this.mirror.refresh(); }, 1);
       this.addEventListeners();
     },
@@ -42,6 +49,48 @@
         this.dispatchEvent(new CustomEvent('content-change',
             {detail: {value: e.getValue()}, bubbles: true}));
       });
+    },
+
+    getCodeMirrorParams(type, value, prefs) {
+      const params = {value};
+
+      if (prefs) {
+        // TODO: Add gerrit's customizations from java codemirror to javascript
+        // gerrit-gwtui/src/main/java/net/codemirror/lib/Extras.java
+        params.autoCloseBrackets = prefs.auto_close_brackets;
+        params.cursorBlinkRate = prefs.cursor_blink_rate;
+        params.cursorHeight = 0.85;
+        params.hideTopMenu = prefs.hide_top_menu;
+        params.indentUnit = prefs.indent_unit;
+        params.indentWithTabs = prefs.indent_with_tabs;
+        params.keyMap = prefs.key_map_type.toLowerCase();
+        params.lineLength = prefs.line_length;
+        params.lineNumbers = prefs.hide_line_numbers;
+        params.lineWrapping = prefs.line_wrapping;
+        params.indentWithTabs = prefs.indent_with_tabs;
+        params.matchBrackets = prefs.match_brackets;
+        // TODO: Add support for a new commit msg MIME type
+        // Support for this is somewhere in gerrit's codebase
+        // needs backporting to javascript
+        params.mode = prefs.syntax_highlighting ? this._mapFileType(type) : '';
+        params.origLeft = value;
+        params.showTabs = prefs.show_tabs;
+        params.showTrailingSpace = prefs.show_whitespace_errors;
+        params.styleSelectedText = true;
+        params.tabSize = prefs.tab_size;
+        params.theme = prefs.theme.toLowerCase();
+
+        if (value && value.includes('\r\n')) {
+          params.lineSeparator = '\r\n';
+        }
+      }
+
+      return params;
+    },
+
+    _mapFileType(type) {
+      const MIME = CodeMirror.findModeByMIME(type);
+      return MIME && MIME.mime || LANGUAGE_MAP[type] || '';
     },
   });
 })();
