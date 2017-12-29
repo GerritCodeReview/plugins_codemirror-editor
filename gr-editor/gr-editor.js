950f80eb918d352738645cdcc170a59a82d897b0
Paladox none <thomasmulhall410@yahoo.com>
2017-12-29 16:12:22 +0000

    Support syntax highlighting
    Also fix an issue where by it was preventing syntax highlighting from
    working.
    
    Change-Id: Icc222cde89eff7a0992fbcaaf8e7bac560347c18

--- gr-editor/gr-editor.js
+++ gr-editor/gr-editor.js
@@ -14,6 +14,44 @@
 (function() {
   'use strict';
 
+  const LANGUAGE_MAP = {
+    /*'application/dart': 'dart',
+    'application/json': 'json',
+    'application/typescript': 'typescript',
+    'application/x-erb': 'erb',
+    'text/css': 'css',
+    'text/html': 'html',
+    'text/javascript': 'javascript',
+    'text/x-c': 'cpp',
+    'text/x-c++src': 'cpp',
+    'text/x-clojure': 'clojure',
+    'text/x-common-lisp': 'lisp',
+    'text/x-csharp': 'csharp',
+    'text/x-csrc': 'cpp',
+    'text/x-d': 'd',
+    'text/x-go': 'go',
+    'text/x-haskell': 'haskell',
+    'text/x-java': 'java',
+    'text/x-kotlin': 'kotlin',
+    'text/x-lua': 'lua',
+    'text/x-markdown': 'markdown',
+    'text/x-objectivec': 'objectivec',
+    'text/x-ocaml': 'ocaml',
+    'text/x-perl': 'perl',*/
+    'text/x-php': 'php',
+    /*'text/x-protobuf': 'protobuf',
+    'text/x-puppet': 'puppet',
+    'text/x-python': 'python',
+    'text/x-ruby': 'ruby',
+    'text/x-rustsrc': 'rust',
+    'text/x-scala': 'scala',
+    'text/x-shell': 'shell',
+    'text/x-sh': 'bash',
+    'text/x-sql': 'sql',
+    'text/x-swift': 'swift',
+    'text/x-yaml': 'yaml',*/
+  };
+
   Polymer({
     is: 'gr-editor',
     /**
@@ -24,15 +62,15 @@
 
     properties: {
       fileContent: String,
+      fileType: String,
       mirror: Object,
       prefs: Object,
     },
 
     attached() {
       this.scopeSubtree(this.$.wrapper, true);
-      this.mirror = CodeMirror(this.$.wrapper, Object.assign({} , {
-        value: this.fileContent,
-      }, this.prefs));
+      const prefs = this.getPreferences(this.prefs);
+      this.mirror = CodeMirror(this.$.wrapper, prefs);
       this.async(() => { this.mirror.refresh(); }, 1);
       this.addEventListeners();
     },
@@ -42,6 +80,54 @@
         this.dispatchEvent(new CustomEvent('content-change',
             {detail: {value: e.getValue()}, bubbles: true}));
       });
+    },
+
+    getPreferences(prefs) {
+      const content = this.fileContent ? this.fileContent : '';
+
+      let pref = {
+        value: content,
+      };
+
+      if (prefs) {
+        pref.autoCloseBrackets = prefs.auto_close_brackets;
+        pref.cursorBlinkRate = prefs.cursor_blink_rate;
+        pref.cursorHeight = 0.85;
+        pref.indentUnit = prefs.indent_unit;
+        pref.keyMap = prefs.key_map_type.toLowerCase();
+        pref.lineNumbers = prefs.hide_line_numbers;
+        pref.lineWrapping = prefs.line_wrapping;
+        pref.indentWithTabs = prefs.indent_with_tabs;
+        pref.matchBrackets = prefs.match_brackets;
+        if (prefs.syntax_highlighting) {
+          pref.mode = this.languageMap();
+        } else {
+          pref.mode = '';
+        }
+        pref.origLeft = content;
+        pref.scrollbarStyle = 'overlay';
+        pref.showTrailingSpace = prefs.show_whitespace_errors;
+        pref.styleSelectedText = true;
+        pref.tabSize = prefs.tab_size;
+        pref.theme = prefs.theme.toLowerCase();
+
+        if (this.fileContent && this.fileContent.includes('\r\n')) {
+          pref.lineSeparator = '\r\n';
+        }
+      }
+
+      return pref;
+    },
+
+    languageMap() {
+      if (this.fileType && CodeMirror.findModeByMIME(this.fileType)) {
+        return CodeMirror.findModeByMIME(this.fileType).mode;
+      } else if (this.fileType && LANGUAGE_MAP[this.fileType]) {
+        return LANGUAGE_MAP[this.fileType];
+      } else {
+        // Return empty string to prevent it from erroring out.
+        return '';
+      }
     },
   });
 })();
