618ba98ef7d02343339437cb484d4cbe256be5f5
Ben Rohlfs <brohlfs@google.com>
2022-12-15 13:01:26 +0100

    Migrate codemirror plugin to TypeScript and Lit
    Release-Notes: skip
    Google-Bug-Id: b/234805062
    Change-Id: I31671da9ceb79d68fc6bf0d91f9fe90d56052e67

---
+++ web/.eslintrc.js
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+__plugindir = 'codemirror-editor/web';
+module.exports = {
+  extends: '../../.eslintrc.js',
+};
