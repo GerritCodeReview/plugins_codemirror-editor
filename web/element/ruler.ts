13dcd3f4673c56e2b1001ef19eb54236943a35e6
paladox <thomasmulhall410@yahoo.com>
2023-02-27 02:36:04 +0000

    Update CodeMirror to V6
    This change depends on Ic9e9ec8fc40a3b1fc022e1f089691e56ef5dcefa
    to add the new dependacies on CodeMirror v6.
    
    Depends-On: Ic9e9ec8fc40a3b1fc022e1f089691e56ef5dcefa
    Change-Id: Ia837c3f2e3c943051a352c85ff3b7b48ab160c0f

---
+++ web/element/ruler.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import {EditorView, ViewPlugin} from '@codemirror/view';
+
+// Kindly provided by https://discuss.codemirror.net/t/how-to-implement-ruler/4616/4
+function generateRulerPlugin() {
+  let width = 0;
+  let rulerElement: HTMLDivElement;
+
+  class RulerPlugin {
+    containerElement: HTMLDivElement;
+
+    constructor(view: EditorView) {
+      this.containerElement = view.dom.appendChild(
+        document.createElement('div')
+      );
+      this.containerElement.style.cssText = `
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        pointer-events: none;
+        overflow: hidden;
+      `;
+      rulerElement = this.containerElement.appendChild(
+        document.createElement('div')
+      );
+      rulerElement.style.cssText = `
+        position: absolute;
+        border-right: 1px solid #dadce0;
+        height: 100%;
+        opacity: 0.7;
+      `;
+      // TODO: This should be equal to the amount of padding on a line.
+      // This value should be extracted from CodeMirror rather than hardcoded.
+      rulerElement.style.width = `4px`;
+    }
+
+    destroy() {
+      rulerElement.remove();
+    }
+  }
+
+  function updateRulerWidth(newWidth: number, defaultWidth: number, force = false) {
+    if ((newWidth !== width || force) && rulerElement) {
+      width = newWidth;
+      rulerElement.style.left = `${width * defaultWidth}px`;
+    }
+  }
+
+  return {rulerPlugin: ViewPlugin.fromClass(RulerPlugin), updateRulerWidth};
+}
+
+const {rulerPlugin, updateRulerWidth} = generateRulerPlugin();
+
+export {rulerPlugin, updateRulerWidth};
