64c703687cbfcf40207184efbd1ed9a12f0b6c39
paladox <thomasmulhall410@yahoo.com>
2023-02-27 02:36:04 +0000

    Update CodeMirror to V6
    This change depends on Ic9e9ec8fc40a3b1fc022e1f089691e56ef5dcefa
    to add the new dependacies on CodeMirror v6.
    
    Depends-On: Ic9e9ec8fc40a3b1fc022e1f089691e56ef5dcefa
    Change-Id: Ia837c3f2e3c943051a352c85ff3b7b48ab160c0f

--- web/gr-editor.ts
+++ web/gr-editor.ts
@@ -3,38 +3,18 @@
  * Copyright 2022 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-import './codemirror-imports';
+
 import '@gerritcodereview/typescript-api/gerrit';
 import {PluginApi} from '@gerritcodereview/typescript-api/plugin';
 import {html, LitElement} from 'lit';
 import {customElement, property, query} from 'lit/decorators.js';
 
-import {CodeMirrorConfig} from './codemirror-imports';
 import {setScriptSrc} from './safe-script';
 
 declare global {
   interface HTMLElementTagNameMap {
     'gr-editor': GrEditor;
   }
-}
-
-/**
- * This is a standard REST API object:
- * https://gerrit-review.googlesource.com/Documentation/rest-api-accounts.html#edit-preferences-info
- *
- * TODO: Add this object to the plugin API.
- */
-interface EditPreferencesInfo {
-  tab_size?: number;
-  line_length?: number;
-  indent_unit?: number;
-  show_tabs?: boolean;
-  show_whitespace_errors?: boolean;
-  syntax_highlighting?: boolean;
-  match_brackets?: boolean;
-  line_wrapping?: boolean;
-  indent_with_tabs?: boolean;
-  auto_close_brackets?: boolean;
 }
 
 /**
@@ -49,7 +29,7 @@ export class GrEditor extends LitElement {
 
   @property({type: Number}) lineNum?: number;
 
-  @property({type: Object}) prefs?: EditPreferencesInfo;
+  @property({type: Object}) prefs?: unknown;
 
   @property({type: Object}) plugin?: PluginApi;
 
@@ -62,7 +42,9 @@ export class GrEditor extends LitElement {
       <codemirror-element
         id="codemirror"
         .lineNum=${this.lineNum}
-        .params=${this.codeMirrorParams()}
+        .prefs=${this.prefs}
+        .fileContent=${this.fileContent}
+        .fileType=${this.fileType}
       >
       </codemirror-element>
     `;
@@ -84,33 +66,5 @@ export class GrEditor extends LitElement {
     script.crossOrigin = 'anonymous';
     setScriptSrc(script, this.plugin?.url() ?? '');
     document.head.appendChild(script);
-  }
-
-  private codeMirrorParams(): CodeMirrorConfig {
-    const params: CodeMirrorConfig = {
-      value: this.fileContent ?? '',
-    };
-
-    if (this.prefs) {
-      params.autoCloseBrackets = this.prefs.auto_close_brackets;
-      params.cursorHeight = 0.85;
-      params.indentUnit = this.prefs.indent_unit;
-      params.indentWithTabs = this.prefs.indent_with_tabs;
-      params.lineNumbers = true;
-      params.lineWrapping = this.prefs.line_wrapping;
-      params.matchBrackets = this.prefs.match_brackets;
-      params.mode = this.prefs.syntax_highlighting ? this.fileType ?? '' : '';
-      params.showTabs = this.prefs.show_tabs;
-      params.showTrailingSpace = this.prefs.show_whitespace_errors;
-      params.tabSize = this.prefs.tab_size;
-      if (this.prefs.line_length) {
-        params.rulers = [{column: this.prefs.line_length}];
-      }
-      if (this.fileContent?.includes('\r\n')) {
-        params.lineSeparator = '\r\n';
-      }
-    }
-
-    return params;
   }
 }
