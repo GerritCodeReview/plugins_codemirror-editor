811bcc9861b0e5c5d2020d5e4b660498ac56086a
Paladox none <thomasmulhall410@yahoo.com>
2025-03-27 17:35:32 -0700

    Create a gerrit theme for CodeMirror
    It highlights more and also tries to align with what we do for highlight.js.
    
    Change-Id: Ie2873fece99f755805afa915fe1f052d531797ed

--- web/element/extensions.ts
+++ web/element/extensions.ts
@@ -98,6 +98,32 @@ const tabTheme = () =>
     },
   });
 
+export const gerritTheme = HighlightStyle.define([
+  { tag: [t.standard(t.tagName), t.tagName], color: 'var(--syntax-tag-color)' },
+  { tag: [t.comment], color: 'var(--syntax-comment-color)' },
+  { tag: [t.className, t.propertyName], color: 'var(--syntax-attr-color)' },
+  { tag: t.variableName, color: 'var(--syntax-variable-color)' },
+  { tag: [t.attributeName, t.operator], color: 'var(--syntax-attr-color)' },
+  { tag: t.number, color: 'var(--syntax-regexp-color)' },
+  { tag: t.keyword, color: 'var(--syntax-keyword-color)' },
+  { tag: [t.typeName, t.typeOperator], color: 'var(--syntax-type-color)' },
+  { tag: t.string, color: 'var(--syntax-string-color)' },
+  { tag: t.regexp, color: 'var(--syntax-regexp-color)' },
+  { tag: t.meta, color: 'var(--syntax-meta-color)' },
+  { tag: [t.name, t.quote], color: 'var(--syntax-title-color)' },
+  { tag: [t.heading, t.strong], color: 'var(--syntax-strong-color)', fontWeight: 'bold' },
+  { tag: [t.emphasis], fontStyle: 'italic' },
+  { tag: [t.deleted], color: '#a11' },
+  { tag: t.inserted, color: '#164' },
+  { tag: t.literal, color: 'var(--syntax-literal-color)' },
+  { tag: [t.atom, t.bool, t.special(t.variableName)], color: 'var(--syntax-literal-color)' },
+  { tag: [t.url, t.escape, t.link], color: 'var(--syntax-string-color)' },
+  { tag: [t.special(t.string)], color: "#e40"},
+  { tag: t.link, textDecoration: 'underline' },
+  { tag: t.strikethrough, textDecoration: 'line-through' },
+  { tag: t.invalid, color: '#f00' },
+]);
+
 export const extensions = (
   height: number,
   prefs?: EditPreferencesInfo,
@@ -152,7 +178,7 @@ export const extensions = (
   if (prefs.syntax_highlighting && language(fileType)) {
     codeExtensions.push(
       language(fileType) as Extension,
-      syntaxHighlighting(defaultHighlightStyle, {fallback: true})
+      syntaxHighlighting(gerritTheme, {fallback: true})
     );
   }
 
